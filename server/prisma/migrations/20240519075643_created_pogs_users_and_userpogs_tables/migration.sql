dfd439903b60a9eb68e7884eca2c43d18b2f6c5d
Franchescka Jewel Puljanan <franchesckajewel.puljanan-22@cpu.edu.ph>
2024-05-19 22:08:04 +0800

    - updated schema.prisma

--- server/prisma/migrations/20240519075643_created_pogs_users_and_userpogs_tables/migration.sql
+++ server/prisma/migrations/20240519075643_created_pogs_users_and_userpogs_tables/migration.sql
@@ -19,7 +19,7 @@ CREATE TABLE "users" (
     "lastName" VARCHAR(255) NOT NULL,
     "position" VARCHAR(255) NOT NULL,
     "email" VARCHAR(255) NOT NULL,
-    "hashedPassword" VARCHAR(10) NOT NULL,
+    "hashedPassword" VARCHAR(60) NOT NULL,
     "eWalletAmount" DECIMAL(19,2) NOT NULL DEFAULT 10000.00,
 
     CONSTRAINT "users_pkey" PRIMARY KEY ("id")
